675d677e9b63dfd8fbc59ab6a9e73e9647645069
Aditya-00001 <adityanjena1@gmail.com>
2026-01-21 00:24:33 +0530

    commented a part

--- frontend/src/components/QuizEditor.jsx
+++ frontend/src/components/QuizEditor.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 export default function QuizEditor({ quiz, setQuiz, onStartQuiz }) {
 
   const updateQuestion = (qIdx, newQuestion) => {
